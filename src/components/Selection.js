b62965e0fc19b63e4a68ce7b9cd8751f6e408a66
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-13 23:59:14 +0530

    check

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React , {useState} from 'react';
 
+
+const Selection=({applyColor})=>{
+
+const [selectionStyle,updateSelectionStyle]=useState({});  
+
+
+console.log(selectionStyle);
+    return (
+      <div className='fix-box' style={selectionStyle} onClick={()=>applyColor(updateSelectionStyle)}>
+         <h1>Selection</h1>
+      </div>
+    )
+}
+
+export default Selection;
